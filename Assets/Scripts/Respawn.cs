80cd2dafd89cbec35e7fafa9fcd62e78f290cd42
Ness Vera <vvness@mit.edu>
2022-10-13 15:57:31 -0400

    implemented death determination with single spike

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -10,8 +10,33 @@ public class Respawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerTransform.position.y <=-10){
-            playerTransform.position = spawnPosition.position;
+        if (playerTransform.position.y <=-10) //fall death
+        {
+            isDead();
         }
+        //clear
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+         isDead();   
+         //if(col.gameObject.tag == "Player")
+         //{
+         //    isDead();
+         //}
+    }
+    
+
+      void OnCollisionEnter2D(Collision2D collision)
+    {
+         if (collision.gameObject.tag == "Player")
+         {
+            isDead();
+         }
+    }
+
+    void isDead()
+    {
+        playerTransform.position = spawnPosition.position;
     }
 }
